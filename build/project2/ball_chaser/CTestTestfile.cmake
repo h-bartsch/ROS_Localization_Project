8e92f98cc1664e92f66a44ea6c1882239f009d3d
h-bartsch <haydenvbartsch@gmail.com>
2020-10-19 17:50:54 -0400

    second try

---
+++ build/project2/ball_chaser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hayden_b/workspace/catkin_ws/src/project2/ball_chaser
+# Build directory: /home/hayden_b/workspace/catkin_ws/build/project2/ball_chaser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ball_chaser_MESSAGE_FILES "")
+set(ball_chaser_SERVICE_FILES "/home/hayden_b/workspace/catkin_ws/src/project2/ball_chaser/srv/DriveToTarget.srv")
